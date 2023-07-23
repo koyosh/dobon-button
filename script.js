90afc1199fa4e97ed290a0feeb1525e5467af1c2
koyoshida <ko.yosh95@gmail.com>
2023-07-23 20:56:15 +0900

    Deleting comment and console.log

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const btn = document.querySelector('#main_button');
 const retryBtn = document.querySelector('#retry_button')
 
-// ドボン確率の初期設定 (単位: %)
 let modProb = 0;
 
-// ドボン済判定
 
 let isDobon = false;
 
-// 押下時の処理
 const pressButton = () => {
     const prob = Math.floor(Math.random() * 100);
     if (!isDobon) {
-        console.log(prob);
         if (prob <= modProb) {
             isDobon = true;
             btn.innerHTML = 'DOBON!';
@@ -24,7 +20,6 @@ const pressButton = () => {
     }
 }
 
-// リトライ処理
 const retry = () => {
     retryBtn.style.display = 'none';
     isDobon = false;
